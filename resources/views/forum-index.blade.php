c02d8a3ceac9b854676066c35c27e27736d7e146
yassirelkhaili <elkhailiyassir@gmail.com>
2024-04-14 12:07:43 +0100

    added threads still need to add a filter

--- resources/views/forum-index.blade.php
+++ resources/views/forum-index.blade.php
@@ -1,7 +1,11 @@
 <x-main_layout>
-    <x-search />
-    <section class="bg-white dark:bg-gray-900 pt-10">
-        <div class="pb-6 px-4 mx-auto max-w-screen-xl sm:pb-10 lg:px-6" id="search-results">
-            <h1 class="text-white">{{$forum->name}}</h1>
+    <section class="bg-white dark:bg-gray-900 pt-24">
+        <div class="pb-8 px-4 mx-auto max-w-screen-xl sm:pb-16 lg:px-6">
+            <div class="max-w-screen-md mb-4 lg:mb-8">
+                <h2 class="mb-4 text-4xl tracking-tight font-bold text-gray-900 dark:text-white">Welcome to {{$forum->name}}</h2>
+                <p class="text-gray-500 sm:text-xl dark:text-gray-400">Below you will find all the different threads related to {{$forum->name}}. Click one and join the conversation</p>
+            </div>
+            @include("layouts.threads")
+        </div>
       </section>
 </x-main_layout>
--- resources/views/layouts/threads.blade.php
+++ resources/views/layouts/threads.blade.php
@@ -1,13 +1,13 @@
-<div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
-    @forelse ($forums as $forum)
-      <a href={{route("forums.show", $forum->id)}} class="hover:bg-gray-800 rounded-md hover:cursor-pointer">
+<div class="space-y-8 grid grid-cols-2 md:gap-12 md:space-y-0">
+    @forelse ($threads as $thread)
+      <a href="#" class="hover:bg-gray-800 rounded-md hover:cursor-pointer">
         <div class="p-5">
             <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full lg:h-12 lg:w-12">
                 <img alt="laravel-logo" loading="lazy" width="30" height="30" decoding="async" data-nimg="1"
                     style="color:transparent" src="https://cdn.cdnlogo.com/logos/l/23/laravel.svg">
             </div>
-            <h3 class="mb-2 text-xl font-bold dark:text-white">{{ $forum->name }}</h3>
-            <p class="text-gray-500 dark:text-gray-400 text-ellipsis">{{ $forum->description }}</p>
+            <h3 class="mb-2 text-xl font-bold dark:text-white">{{ $thread->name }}</h3>
+            <p class="text-gray-500 dark:text-gray-400 text-ellipsis">{{ $thread->description }}</p>
         </div>
       </a>
     @empty
@@ -16,12 +16,12 @@
                 <img alt="404-logo" class="w-10 h-10" loading="lazy" decoding="async" data-nimg="1"
                     src={{ asset('./assets/svgs/404-error.svg') }}>
             </div>
-            <h3 class="mb-2 text-xl font-bold dark:text-white">Oups no forums were found</h3>
+            <h3 class="mb-2 text-xl font-bold dark:text-white">Oups no threads were found</h3>
             <p class="text-gray-500 dark:text-gray-400">If you encounter this message then a mistake must have occured,
                 please refresh the page and try again</p>
         </div>
     @endforelse
 </div>
 <div class="mt-4">
-    {{ $forums->links() }}
+    {{ $threads->links() }}
 </div>
